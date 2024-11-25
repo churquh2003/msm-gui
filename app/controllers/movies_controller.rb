6ef89a7ae7342c2d5782b58293c12f25b7e768a5
Charlotte Urquhart <curquhart@uchicago.edu>
2024-11-25 08:48:47 +0000

    save7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,27 +1,51 @@
 class MoviesController < ApplicationController
   def index
-    @list_of_movies = Movie.all.order({ :created_at => :desc })
+    matching_movies = Movie.all
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
     render({ :template => "movie_templates/index" })
   end
 
-  def show
-    the_id = params.fetch("path_id")
-    @the_movie = Movie.where({ :id => the_id }).at(0)
-    render({ :template => "movie_templates/show" })
-  end
-
   def create_row
-    movie = Movie.new
-    movie.title = params.fetch("title_movie")
-    movie.year = params.fetch("year_movie")
-    movie.duration = params.fetch("duration_movie")
-    movie.description = params.fetch("description_movie")
-    movie.image = params.fetch("image_movie")
-    movie.director_id = params.fetch("director_id")
-    movie.save
+    m = Movie.new
+    m.title = params.fetch("title_movie")
+    m.year = params.fetch("year_movie")
+    m.duration = params.fetch("duration_movie")
+    m.description = params.fetch("description_movie")
+    m.image = params.fetch("image_movie")
+    m.director_id = params.fetch("director_id")
+    m.save
+
     redirect_to("/movies")
   end
 
   def update_row
     the_id = params.fetch("path_id")
-    movie = Movie.where({ :id => the_id }).at(
+    m = Movie.where({ :id => the_id }).at(0)
+
+    m.title = params.fetch("title_movie")
+    m.year = params.fetch("year_movie")
+    m.duration = params.fetch("duration_movie")
+    m.description = params.fetch("description_movie")
+    m.image = params.fetch("image_movie")
+    m.director_id = params.fetch("director_id")
+    m.save
+
+    redirect_to("/movies/#{m.id}")
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+    @the_movie = Movie.where({ :id => the_id }).at(0)
+
+    render({ :template => "movie_templates/show" })
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    m = Movie.where({ :id => the_id }).at(0)
+    m.destroy
+
+    redirect_to("/movies")
+  end
+end
