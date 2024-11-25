0b5d044a6324d06f69693c4e7622c1eb7a21c815
Charlotte Urquhart <curquhart@uchicago.edu>
2024-11-25 08:37:30 +0000

    save7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,29 +6,48 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/index" })
   end
 
-  def create_row 
-    # Get the user's input out of params
-    # Create and save a new row in items table 
-    # send user to / page
-
+  def create_row
     m = Movie.new
     m.title = params.fetch("title_movie")
     m.year = params.fetch("year_movie")
     m.duration = params.fetch("duration_movie")
     m.description = params.fetch("description_movie")
     m.image = params.fetch("image_movie")
-    m.id = params.fetch("director_id")
+    m.director_id = params.fetch("director_id") # Fixed field
     m.save
 
-    redirect_to("/movies", allow_other_host: true)
-  end 
+    redirect_to("/movies")
+  end
 
   def show
     the_id = params.fetch("path_id")
-
     matching_movies = Movie.where({ :id => the_id })
     @the_movie = matching_movies.at(0)
 
     render({ :template => "movie_templates/show" })
+  end
+
+  def update_row
+    the_id = params.fetch("path_id")
+    m = Movie.where({ :id => the_id }).at(0)
+
+    m.title = params.fetch("title_movie")
+    m.year = params.fetch("year_movie")
+    m.duration = params.fetch("duration_movie")
+    m.description = params.fetch("description_movie")
+    m.image = params.fetch("image_movie")
+    m.director_id = params.fetch("director_id")
+    m.save
+
+    redirect_to("/movies/#{m.id}")
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    matching_movies = Movie.where({ :id => the_id })
+    the_movie = matching_movies.at(0)
+    the_movie.destroy
+
+    redirect_to("/movies")
   end
 end
